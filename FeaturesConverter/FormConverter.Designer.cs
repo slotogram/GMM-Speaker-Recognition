41b33964ce8793435460d2193642c87fcbe81c76
slotogram <slotogram@gmail.com>
2016-02-12 15:17:39 +0600

    Added features converter project
    It creates feature list files now

---
+++ FeaturesConverter/FormConverter.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace FeaturesConverter
+{
+    partial class FormConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox28 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label29 = new System.Windows.Forms.Label();
+            this.textBoxSpeakerSet = new System.Windows.Forms.TextBox();
+            this.label28 = new System.Windows.Forms.Label();
+            this.textBoxUBMPhrases = new System.Windows.Forms.TextBox();
+            this.label27 = new System.Windows.Forms.Label();
+            this.textBoxTestPhrases = new System.Windows.Forms.TextBox();
+            this.label26 = new System.Windows.Forms.Label();
+            this.textBoxLearnPhrases = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.textBoxFeatureExtension = new System.Windows.Forms.TextBox();
+            this.textBoxLists = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUBMSpeakersNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Checked = true;
+            this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox6.Location = new System.Drawing.Point(295, 27);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(105, 17);
+            this.checkBox6.TabIndex = 47;
+            this.checkBox6.Text = "Поддиректории";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.Visible = false;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(12, 267);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(105, 22);
+            this.button13.TabIndex = 46;
+            this.button13.Text = "Создать списки";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(12, 23);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(141, 20);
+            this.textBox21.TabIndex = 45;
+            this.textBox21.Text = "E:\\temp\\123\\12samplesN";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(159, 23);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(37, 23);
+            this.button12.TabIndex = 44;
+            this.button12.Text = "Dir";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // textBox28
+            // 
+            this.textBox28.Location = new System.Drawing.Point(10, 83);
+            this.textBox28.Name = "textBox28";
+            this.textBox28.Size = new System.Drawing.Size(61, 20);
+            this.textBox28.TabIndex = 64;
+            this.textBox28.Text = "0-12";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(79, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(187, 17);
+            this.checkBox1.TabIndex = 66;
+            this.checkBox1.Text = "Используемые характеристики";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.Location = new System.Drawing.Point(76, 217);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(71, 13);
+            this.label29.TabIndex = 99;
+            this.label29.Text = "№ Дикторов";
+            // 
+            // textBoxSpeakerSet
+            // 
+            this.textBoxSpeakerSet.Location = new System.Drawing.Point(10, 217);
+            this.textBoxSpeakerSet.Name = "textBoxSpeakerSet";
+            this.textBoxSpeakerSet.Size = new System.Drawing.Size(47, 20);
+            this.textBoxSpeakerSet.TabIndex = 98;
+            this.textBoxSpeakerSet.Text = "16-25";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Location = new System.Drawing.Point(76, 195);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(107, 13);
+            this.label28.TabIndex = 97;
+            this.label28.Text = "№ файлов для UBM";
+            // 
+            // textBoxUBMPhrases
+            // 
+            this.textBoxUBMPhrases.Location = new System.Drawing.Point(10, 195);
+            this.textBoxUBMPhrases.Name = "textBoxUBMPhrases";
+            this.textBoxUBMPhrases.Size = new System.Drawing.Size(47, 20);
+            this.textBoxUBMPhrases.TabIndex = 96;
+            this.textBoxUBMPhrases.Text = "1-50";
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Location = new System.Drawing.Point(76, 169);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(153, 13);
+            this.label27.TabIndex = 95;
+            this.label27.Text = "№ файлов для тестирования";
+            // 
+            // textBoxTestPhrases
+            // 
+            this.textBoxTestPhrases.Location = new System.Drawing.Point(10, 169);
+            this.textBoxTestPhrases.Name = "textBoxTestPhrases";
+            this.textBoxTestPhrases.Size = new System.Drawing.Size(47, 20);
+            this.textBoxTestPhrases.TabIndex = 94;
+            this.textBoxTestPhrases.Text = "41-50";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(76, 143);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(129, 13);
+            this.label26.TabIndex = 93;
+            this.label26.Text = "№ файлов для обучения";
+            // 
+            // textBoxLearnPhrases
+            // 
+            this.textBoxLearnPhrases.Location = new System.Drawing.Point(10, 143);
+            this.textBoxLearnPhrases.Name = "textBoxLearnPhrases";
+            this.textBoxLearnPhrases.Size = new System.Drawing.Size(47, 20);
+            this.textBoxLearnPhrases.TabIndex = 92;
+            this.textBoxLearnPhrases.Text = "1-40";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(76, 118);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(92, 13);
+            this.label16.TabIndex = 91;
+            this.label16.Text = "Расширение фич";
+            // 
+            // textBoxFeatureExtension
+            // 
+            this.textBoxFeatureExtension.Location = new System.Drawing.Point(10, 115);
+            this.textBoxFeatureExtension.Name = "textBoxFeatureExtension";
+            this.textBoxFeatureExtension.Size = new System.Drawing.Size(47, 20);
+            this.textBoxFeatureExtension.TabIndex = 90;
+            this.textBoxFeatureExtension.Text = ".htk";
+            // 
+            // textBoxLists
+            // 
+            this.textBoxLists.Location = new System.Drawing.Point(12, 52);
+            this.textBoxLists.Name = "textBoxLists";
+            this.textBoxLists.Size = new System.Drawing.Size(141, 20);
+            this.textBoxLists.TabIndex = 101;
+            this.textBoxLists.Text = "E:\\temp\\123\\12samplesN\\Lists";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 23);
+            this.button1.TabIndex = 100;
+            this.button1.Text = "Dir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 103;
+            this.label1.Text = "№ Дикторов для UBM";
+            // 
+            // textBoxUBMSpeakersNum
+            // 
+            this.textBoxUBMSpeakersNum.Location = new System.Drawing.Point(10, 239);
+            this.textBoxUBMSpeakersNum.Name = "textBoxUBMSpeakersNum";
+            this.textBoxUBMSpeakersNum.Size = new System.Drawing.Size(47, 20);
+            this.textBoxUBMSpeakersNum.TabIndex = 102;
+            this.textBoxUBMSpeakersNum.Text = "1-15";
+            // 
+            // FormConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 315);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUBMSpeakersNum);
+            this.Controls.Add(this.textBoxLists);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label29);
+            this.Controls.Add(this.textBoxSpeakerSet);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.textBoxUBMPhrases);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.textBoxTestPhrases);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.textBoxLearnPhrases);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.textBoxFeatureExtension);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox28);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.button12);
+            this.Name = "FormConverter";
+            this.Text = "Features converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox28;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.TextBox textBoxSpeakerSet;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.TextBox textBoxUBMPhrases;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.TextBox textBoxTestPhrases;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.TextBox textBoxLearnPhrases;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox textBoxFeatureExtension;
+        private System.Windows.Forms.TextBox textBoxLists;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUBMSpeakersNum;
+    }
+}
+
---
+++ FeaturesConverter/FormConverter.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+/*
+ * 
+ * Что должен делать конвертер:
+ * 1. Создавать списки с файлами для дикторов
+ * 2. Убирать ненужные фичи из файлов
+ * 3. Объединять файлы с фичами в один (обучающая выборка) или в несколько (тестовая выборка)
+ * 4. Убирать ненужные вреймы (на основе VAD)
+ */
+namespace FeaturesConverter
+{
+    public partial class FormConverter : Form
+    {
+        public FormConverter()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBox21.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void OutList(string path, List<string> fileList)
+        {
+            StreamWriter fs = new StreamWriter(Environment.CurrentDirectory + "\\" + path + ".txt");
+            foreach (string s in fileList)
+            {
+                fs.WriteLine(s);
+            }
+            fs.Close();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //Парсим текстбокс с номерами характеристик
+            List<int> feat_num = stringNumParse(textBox28.Text, true);
+
+            //Парсим текстбокс со списком файлов, используемых для обучения моделей дикторов
+            List<int> learn_num = stringNumParse(textBoxLearnPhrases.Text, false);
+            //Парсим текстбокс со списком файлов, используемых для тестирования моделей дикторов
+            List<int> test_num = stringNumParse(textBoxTestPhrases.Text, false);
+            //Парсим текстбокс со списком файлов, используемых для создания UBM
+            List<int> UBM_num = stringNumParse(textBoxUBMPhrases.Text, false);
+            List<int> UBM_Speakers = stringNumParse(textBoxUBMSpeakersNum.Text, false);
+            //Парсим текстбокс со списком speakers
+            List<int> Speakers = stringNumParse(textBoxSpeakerSet.Text, false);
+
+            string featPath = textBoxLists.Text;
+
+            //получаем список нужных нам файлов с фичами и выводим их в текстовые лист файлы
+
+            /*  string[] s;
+                if (checkBox6.Checked)
+                s = System.IO.Directory.GetFiles(featPath, "*" + textBoxFeatureExtension.Text, SearchOption.AllDirectories);
+            else s = System.IO.Directory.GetFiles(featPath, "*"+textBoxFeatureExtension.Text);*/
+
+            List<string> filesList = new List<string>();
+            //создаем список для UBM
+            foreach (int i in UBM_num)
+            {
+                foreach (int j in UBM_Speakers)
+                {
+                    filesList.Add(j + " (" + i + ")" + textBoxFeatureExtension.Text);
+                }
+            }
+            //выводим в файл UBM.lst
+            OutList(featPath+"\\UBM.lst",filesList);
+            //создаем список обучающих файлов
+
+            filesList.Clear();
+            foreach (int j in Speakers)
+            {   
+                foreach (int i in learn_num)
+                {
+                    filesList.Add(j+ " "+j + " (" + i + ")" + textBoxFeatureExtension.Text);
+                }
+            }
+            //выводим в файл Train.lst
+            OutList(featPath + "\\Train.lst", filesList);
+            //создаем список тестовых файлов
+            filesList.Clear();
+            foreach (int j in Speakers)
+            {
+                foreach (int k in Speakers)
+                {
+                    foreach (int i in learn_num)
+                    {
+                        
+                        filesList.Add(j+ " " + k + " (" + i + ")" + textBoxFeatureExtension.Text+ " "+ ((j == k) ? "1" : "0"));
+                    }
+                }
+                
+            }
+            //выводим в файл Test.lst
+            OutList(featPath + "\\Test.lst", filesList);
+        }
+
+        private List<int> stringNumParse(string s, bool needZeroEnd)
+        {
+            string[] numbers = s.Replace(',', ' ').Split(' ');
+            List<int> feat_num = new List<int>();
+            foreach (string s1 in numbers)
+            {
+                if (!s1.Contains(' '))
+                {
+                    if (s1.Contains('-'))
+                    {
+                        int i1 = 0, i2 = 0;
+                        int.TryParse(s1.Split('-')[0], out i1);
+                        int.TryParse(s1.Split('-')[1], out i2);
+                        if (i2 != 0)
+                            for (int i = i1; i <= i2; i++) feat_num.Add(i);
+                    }
+                    else
+                    {
+                        int i = 0;
+                        int.TryParse(s1, out i);
+                        if (i != 0) feat_num.Add(i);
+                        if (s1 == "0") feat_num.Add(0);
+                    }
+                }
+            }
+            if (feat_num.Count == 0) feat_num = null;
+            else if (needZeroEnd) feat_num.Add(0);
+
+            return feat_num;
+        }
+
+    }
+}
